4a2f0372b4beb9d317ab16696b9045048015f75c
Tom Noble <tom.noble@bjss.com>
2021-04-04 20:13:10 +0100

    Update s3.py

--- awssert/s3.py
+++ awssert/s3.py
@@ -1,8 +1,17 @@
 from awssert.keywords import keywords
+from awssert.core import KeywordRouter, BotoObjectProxyRegister, BotoObjectProxy
 
 
 class BucketAssertions:
 
-    @keywords(["should", "shouldnt", "should_not", "does", "doesnt", "does_not"])
-    def contain(self, bucket, object):
-        return object in bucket.objects.filter(object)
+    @keywords(["should", "does"])
+    def contain(self, bucket, key):
+        objects = bucket.objects.filter(Prefix=key)
+        assert any([key == object.key for object in objects])
+
+
+def register_s3_assertions(class_attributes, base_classes, **kwargs):
+    proxy = BotoObjectProxy()
+    base_classes.insert(0, BotoObjectProxyRegister)
+    class_attributes['proxy'] = proxy
+    class_attributes['should'] = KeywordRouter("should", BucketAssertions(), proxy)
