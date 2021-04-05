0cb9844755d5c3b72e4066def12e61da9f89e94a
Tom Noble <tom.noble@bjss.com>
2021-04-05 15:50:45 +0100

    Add flake8 config

--- awssert/__init__.py
+++ awssert/__init__.py
@@ -1,4 +1,4 @@
-import pkg_resources
+# import pkg_resources
 
 # TODO: Fix pkg_resources.DistributionNotFound error when running through Github Actions
 # __version__ = pkg_resources.get_distribution("awssert").version
