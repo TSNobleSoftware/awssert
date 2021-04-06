2700190cf9fd162329b09278f97b2178591c253f
ZestReleaser <N/A>
2021-04-06 12:05:26 +0000

    Preparing release 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-version = "0.0.5.dev0"
+version = "0.0.5"
 here = Path(__file__).parent.resolve()
 
 with open(here.joinpath("README.md"), encoding="utf-8") as f:
