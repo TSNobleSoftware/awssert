f5df916a3b281d76f5b06e65f523cea0b491e7b9
Tom Noble <tom.noble@bjss.com>
2021-04-06 12:29:34 +0100

    Replace awssert fixture with pytest hook

--- awssert/__init__.py
+++ awssert/__init__.py
@@ -2,3 +2,5 @@
 
 # TODO: Fix pkg_resources.DistributionNotFound error when running through Github Actions
 # __version__ = pkg_resources.get_distribution("awssert").version
+
+from awssert.hooks import *
