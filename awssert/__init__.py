5d34a6b35eab708c34b65cd9a6cedba6b0766256
TSNoble <tom.noble@bjss.com>
2021-04-09 18:34:48 +0100

    Uncomment __version__ attribute

--- awssert/__init__.py
+++ awssert/__init__.py
@@ -1,6 +1,5 @@
-# import pkg_resources
+import pkg_resources
 
-# TODO: Fix pkg_resources.DistributionNotFound error when running through Github Actions
-# __version__ = pkg_resources.get_distribution("awssert").version
+__version__ = pkg_resources.get_distribution("awssert").version
 
 from awssert.hooks import *
