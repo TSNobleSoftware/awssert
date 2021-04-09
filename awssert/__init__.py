c74edcacf8b01fd490e3937c729cbb31f8a031ba
Tom Noble <53005340+TSNoble@users.noreply.github.com>
2021-04-09 18:58:38 +0100

    Fix __version__ module attribute breaking tests in workflow

--- awssert/__init__.py
+++ awssert/__init__.py
@@ -1,6 +1,8 @@
-# import pkg_resources
+import pkg_resources
 
-# TODO: Fix pkg_resources.DistributionNotFound error when running through Github Actions
-# __version__ = pkg_resources.get_distribution("awssert").version
+try:
+    __version__ = pkg_resources.get_distribution("awssert").version
+except pkg_resources.DistributionNotFound:
+    pass
 
 from awssert.hooks import *
