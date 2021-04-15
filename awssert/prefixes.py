bbf0f22ec08be3993147a725a7975aff0a99c1cc
TSNoble <tom.noble@bjss.com>
2021-04-15 07:48:52 +0100

    Split decorator into positives and negatives, only register relevant prefies, and remove restrictions on valid prefies

--- awssert/prefixes.py
+++ awssert/prefixes.py
@@ -1,58 +1,64 @@
-from awssert.exceptions import PrefixNotSupportedError
-
-
 class AssertionPrefixes:
 
-    _register = {}
-    positive = [
-        "should",
-        "does",
-        "should_be",
-        "should_have",
-        "has",
-        "belongs_to",
-        "is_part_of",
-        "was",
-    ]
-    negative = [
-        "should_not",
-        "does_not",
-        "does_not_have",
-        "should_not_be",
-        "should_not_have",
-        "is_not_part_of",
-        "does_not_belong_to",
-        "was_not",
-    ]
-    all = positive + negative
+    _positives = {}
+    _negatives = {}
 
     @classmethod
-    def register_method(cls, method, allowed_prefixes):
+    def register_positives(cls, method, allowed_prefixes):
         class_of_method = method.__qualname__.split(".")[0]
-        if class_of_method not in cls._register:
-            cls._register_class(class_of_method)
+        if class_of_method not in cls._positives:
+            cls._register_class(class_of_method, cls._positives)
         for prefix in allowed_prefixes:
-            cls._register_class_method(class_of_method, prefix, method)
+            cls._register_class_method(class_of_method, prefix, method, cls._positives)
+
+    @classmethod
+    def register_negatives(cls, method, allowed_prefixes):
+        class_of_method = method.__qualname__.split(".")[0]
+        if class_of_method not in cls._negatives:
+            cls._register_class(class_of_method, cls._negatives)
+        for prefix in allowed_prefixes:
+            cls._register_class_method(class_of_method, prefix, method, cls._negatives)
 
     @classmethod
     def get_methods_allowing_prefix(cls, on_class, prefix):
-        return cls._register[on_class.__name__][prefix]
+        positives = cls._positives[on_class.__name__].get(prefix, {})
+        negatives = cls._negatives[on_class.__name__].get(prefix, {})
+        return {**positives, **negatives}
 
     @classmethod
-    def _register_class(cls, class_name):
-        cls._register[class_name] = {prefix: {} for prefix in cls.all}
+    def get_prefixes(cls, on_class):
+        return cls.get_positives(on_class) + cls.get_negatives(on_class)
 
     @classmethod
-    def _register_class_method(cls, class_name, keyword, method):
-        try:
-            cls._register[class_name][keyword][method.__name__] = method
-        except KeyError:
-            raise PrefixNotSupportedError(f"Keyword '{keyword}' is not supported")
+    def get_positives(cls, on_class):
+        return list(cls._positives[on_class.__name__].keys())
+
+    @classmethod
+    def get_negatives(cls, on_class):
+        return list(cls._negatives[on_class.__name__].keys())
+
+    @classmethod
+    def _register_class(cls, class_name, register):
+        register[class_name] = {}
+
+    @classmethod
+    def _register_class_method(cls, class_name, keyword, method, register):
+        if keyword not in register[class_name]:
+            register[class_name][keyword] = {}
+        register[class_name][keyword][method.__name__] = method
 
 
-def prefixes(allowed_prefixes):
+def positives(allowed_prefixes):
     def registrar(method):
-        AssertionPrefixes.register_method(method, allowed_prefixes)
+        AssertionPrefixes.register_positives(method, allowed_prefixes)
+        return method
+
+    return registrar
+
+
+def negatives(allowed_prefixes):
+    def registrar(method):
+        AssertionPrefixes.register_negatives(method, allowed_prefixes)
         return method
 
     return registrar
