834d568e34792897613b298e8711b8a0e281e81d
Tom Noble <53005340+TSNoble@users.noreply.github.com>
2021-04-15 18:56:17 +0100

    Pass positives and negatives as *args rather than list

--- awssert/dynamodb.py
+++ awssert/dynamodb.py
@@ -5,18 +5,18 @@ class TableAssertions:
 
     attaches_to = "dynamodb.Table"
 
-    @positives(["should_be"])
-    @negatives(["should_not_be"])
+    @positives("should_be")
+    @negatives("should_not_be")
     def empty(self, table):
         return table.item_count == 0
 
-    @positives(["should_have", "has"])
-    @negatives(["should_not_have", "does_not_have"])
+    @positives("should_have", "has")
+    @negatives("should_not_have", "does_not_have")
     def item(self, table, key):
         return "Item" in table.get_item(Key=key)
 
-    @positives(["should_have", "has"])
-    @negatives(["should_not_have", "does_not_have"])
+    @positives("should_have", "has")
+    @negatives("should_not_have", "does_not_have")
     def key(self, table, key):
         keys = [key["AttributeName"] for key in table.key_schema]
         return key in keys
