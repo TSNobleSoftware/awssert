560232cef46861aa149afd14124ef524875b2d54
TSNoble <tom.noble@bjss.com>
2021-04-15 18:53:48 +0100

    Pass positives and negatives as *args rather than list

--- awssert/s3.py
+++ awssert/s3.py
@@ -5,18 +5,18 @@ class BucketAssertions:
 
     attaches_to = "s3.Bucket"
 
-    @positives(["should", "does"])
-    @negatives(["should_not", "does_not"])
+    @positives("should", "does")
+    @negatives("should_not", "does_not")
     def contain(self, bucket, key):
         objects = bucket.objects.filter(Prefix=key)
         return any([key == object.key for object in objects])
 
-    @positives(["should_be"])
-    @negatives(["should_not_be"])
+    @positives("should_be")
+    @negatives("should_not_be")
     def empty(self, bucket):
         return len(list(bucket.objects.all())) == 0
 
-    @positives(["should", "does"])
-    @negatives(["should_not", "does_not"])
+    @positives("should", "does")
+    @negatives("should_not", "does_not")
     def exist(self, bucket):
         return bucket.creation_date is not None
