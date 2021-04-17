747ba4977b095f3965ec8a3b8b2f45398c132db2
TSNoble <tom.noble@bjss.com>
2021-04-17 20:15:57 +0100

    Add better Assertion failure messages

--- awssert/dynamodb.py
+++ awssert/dynamodb.py
@@ -8,15 +8,15 @@ class TableAssertions:
     @positives("should_be")
     @negatives("should_not_be")
     def empty(self, table):
-        assert table.item_count == 0
+        return table.item_count == 0
 
     @positives("should_have", "has")
     @negatives("should_not_have", "does_not_have")
     def item(self, table, key):
-        assert "Item" in table.get_item(Key=key)
+        return "Item" in table.get_item(Key=key)
 
     @positives("should_have", "has")
     @negatives("should_not_have", "does_not_have")
     def key(self, table, key):
         keys = [key["AttributeName"] for key in table.key_schema]
-        assert key in keys
+        return key in keys
--- awssert/s3.py
+++ awssert/s3.py
@@ -10,14 +10,14 @@ class BucketAssertions:
     def contain(self, bucket, object):
         key = object if isinstance(object, str) else object.key
         objects = bucket.objects.filter(Prefix=key)
-        assert any([key == obj.key for obj in objects])
+        return any([key == obj.key for obj in objects])
 
     @positives("should_be")
     @negatives("should_not_be")
     def empty(self, bucket):
-        assert len(list(bucket.objects.all())) == 0
+        return len(list(bucket.objects.all())) == 0
 
     @positives("should", "does")
     @negatives("should_not", "does_not")
     def exist(self, bucket):
-        assert bucket.creation_date is not None
+        return bucket.creation_date is not None
